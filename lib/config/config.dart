04011248b8fe680d10877759f2b37134fe98a5a5
riakgu <rizkyakbargusnaen@gmail.com>
2024-05-25 17:18:48 +0700

    Add local and production url api

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,3 +1,6 @@
 class Config {
-  static const String baseUrl = 'https://gasstugas.my.id/api';
+  static const String local = 'http://192.168.241.223:8000/api';
+  static const String production = 'https://gasstugas.my.id/api';
+
+  static const String baseUrl = production;
 }
