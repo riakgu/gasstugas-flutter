87922ec91969df9b7574a63d2daf7f0d5c4e981e
riakgu <rizkyakbargusnaen@gmail.com>
2024-06-10 21:15:04 +0700

    Update config

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,6 +1,3 @@
 class Config {
-  static const String local = 'http://192.168.241.223:8000/api';
-  static const String production = 'https://gasstugas.my.id/api';
-
-  static const String baseUrl = production;
+  static const String baseUrl = 'https://gasstugas.my.id/api';
 }
